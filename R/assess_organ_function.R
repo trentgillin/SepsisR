0071ab6378a0551a615f3aa207a5d630f0a3a097
trentgillin <trentgillin@gmail.com>
2019-02-23 10:48:47 -0500

    created two new functions related to organ dysfunction

---
+++ R/assess_organ_function.R
@@ -0,0 +1,22 @@
+#' Assess Organ Failure Function
+#'
+#' Finds the current organ failure score.
+#'
+#' @param .data The dataset you are working with.
+#' @param method A character value indicating what method you would like to use to score organ failure.
+#' @examples
+#' score <- assess_organ(dataset, method = "SIRS")
+#' score <- assess_rogan(dataset, method = "SOFA")
+#' @export
+
+assess_organ <- function(.data, method = "SIRS") {
+  if (method == "SIRS") {
+    new_data <- find_sirs(.data, patientid)
+  } else if (method == "SOFA") {
+    new_data <- find_sofa(.data)
+  } else {
+    new_data <- find_qsofa(.data)
+  }
+
+  return(new_data)
+}
