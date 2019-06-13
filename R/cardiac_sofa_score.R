a6ef3c66a51c850494e8b7815887da5e9233de02
trentgillin <trentgillin@gmail.com>
2019-06-13 19:53:58 -0400

    added a new function

--- R/cardiac_sofa_score.R
+++ R/cardiac_sofa_score.R
@@ -8,8 +8,9 @@
 #' @examples
 #' result <- get_map(sbp, dbp)
 
-get_map <- function(sbp, dpb) {
-  map <- (sbp + (2 *dbp))/3
+get_map <- function(sbp, dbp) {
+  map <- (sbp + (2 * dbp))/3
+  return(map)
 }
 
 #' SOFA Cardiac calculation function
@@ -20,14 +21,20 @@ get_map <- function(sbp, dpb) {
 #' @param sbp A numeric column that represents the systolic blood pressure
 #' @param dbp A numeric column that represents the dystolic blood pressure
 #' @param vasopressor A character column that represents a list of vasopressors given to a patient
-#' @param vasopressor_dose A numeric column that represents the dosage of vasopressor given
+#' @param vasopressor_dose A numeric column that represents the dosage of vasopressor given, needs to be in mcg/kg/min
 #' @examples
 #' cardiovascular_flag = calc_card_sofa(.data, sbp, dbp, vasopressor, vasopressor_dose)
-#' @export
 
-calc_card_sofa <- function(.data, sbp, dpb, vasopressor, vasopressor_dose) {
-  .data <- mutate(.data, MAP = get_map(sbp, dpb))
+calc_card_sofa <- function(.data, sbp, dbp, vasopressor, vasopressor_dose) {
+  # get map
+  .data <- mutate(.data, MAP = get_map(sbp, dbp))
 
+  # create cardiac flag
+  .data <- mutate(.data, cardiac_flag = case_when(MAP < 70 ~ 1,
+                                                  str_detect(str_to_lower(vasopressor), "dobutamine")|
+                                                    (str_detect(str_to_lower(vasopressor), "dopamine") & vasopressor_dose <= 5)~2,
+                                                  ))
+  return(.data)
 }
 
 
