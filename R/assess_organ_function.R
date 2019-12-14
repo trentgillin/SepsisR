29e75423123b45c1bfd16a04134b0301da165e66
trentgillin <trentgillin@gmail.com>
2019-12-13 21:47:49 -0500

    made it so you can call all sirs, sofa, and qsofa with on function

--- R/assess_organ_function.R
+++ R/assess_organ_function.R
@@ -4,19 +4,44 @@
 #'
 #' @param .data The dataset you are working with.
 #' @param method A character value indicating what method you would like to use to score organ failure.
+#' @param patientid A character or numeric value unique to each patient.
+#' @param time A POSIXct value that represents when a vital was taken.
+#' @param vitals A character vector denoting the certain vitals necessary for each organ failure score.
 #' @examples
-#' score <- assess_organ(dataset, method = "SIRS")
-#' score <- assess_rogan(dataset, method = "SOFA")
+#' score <- assess_organ(dataset, method = "SIRS", patientid = Encounter, time = Service_Timestamp, vitals = c("RR" = NA,"SBP" = NA,"GCS" = NA))
 #' @export
 
-assess_organ <- function(.data, method = "SIRS") {
-  if (method == "SIRS") {
-    new_data <- find_sirs(.data, patientid = encounter, time = service_timestamp, period = 1)
-  } else if (method == "SOFA") {
-    new_data <- find_sofa(.data)
-  } else {
-    new_data <- find_qsofa(.data)
-  }
+assess_organ <- function(.data, method = c("SIRS", "SOFA", "qSOFA"),
+                         patientid,
+                         time,
+                         period = 1,
+                         vitals) {
 
-  return(new_data)
+  patientid <- enquo(patientid)
+  time <- enquo(time)
+
+  method <- match.arg(method)
+  switch(method,
+         SIRS = {
+           new_data <- find_sirs(.data,
+                          !!patientid,
+                          !!time,
+                          period,
+                          vitals)},
+
+         SOFA = {
+           new_data <- find_SOFA(.data,
+                          !!patientid,
+                          !!time,
+                          period,
+                          vitals)},
+
+         qSOFA = {
+           new_data <-find_qsofa(.data,
+                            !!patientid,
+                            !!time,
+                            period,
+                            vitals)})
+
+         return(new_data)
 }
